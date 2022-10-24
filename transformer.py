1956d5342a80ab253189698b0318edac833ada94
rin-kinokocan <rin1702-se@kinoko-can.com>
2022-10-25 01:12:06 +0900

    Did some test and fixed attentions.

--- transformer.py
+++ transformer.py
@@ -7,13 +7,15 @@ class Transformer(nn.Module):
         super().__init__()
 
 
-def Attention(query, key, value, is_masked=false):
+def Attention(query, key, value, is_masked=False):
     """Attention mechanism"""
-    weight = torch.softmax(
-        torch.einsum("ihk,jhk->ihj", query, key)/torch.sqrt(query.size()[1]))
+    scale = torch.sqrt(torch.tensor([query.size()[1]]))
+    h = torch.einsum("ihk,jhk->ihj", query, key)/scale
+    weight = nn.functional.softmax(h, dim=0)
     if is_masked:
         pass
     return torch.einsum("ihj,jhv->ihv", weight, value)
+
 
 class MultiheadAttention(nn.Module):
     """Multihead Attention layer"""
@@ -21,20 +23,20 @@ class MultiheadAttention(nn.Module):
     def __init__(self, h_num, model_dim, k_dim=None, v_dim=None) -> None:
         super().__init__()
         if k_dim == None:
-            k_dim = model_dim/h_num
+            k_dim = max((int)(model_dim/h_num), 1)
         if v_dim == None:
-            v_dim = model_dim/h_num
+            v_dim = max((int)(model_dim/h_num), 1)
         self.h_num = h_num
         self.model_dim = model_dim
         self.v_dim = v_dim
-        self.q_w = torch.tensor(torch.random(model_dim, k_dim, h_num),
-                                requires_grad=True)
-        self.k_w = torch.tensor(torch.random(model_dim, k_dim, h_num),
-                                requires_grad=True)
-        self.v_w = torch.tensor(torch.random(model_dim, v_dim, h_num),
-                                requires_grad=True)
-        self.o_w = torch.tensor(torch.random(h_num * v_dim, model_dim, h_num),
-                                requires_grad=True)
+        self.q_w = torch.rand(model_dim, k_dim, h_num,
+                              requires_grad=True)
+        self.k_w = torch.rand(model_dim, k_dim, h_num,
+                              requires_grad=True)
+        self.v_w = torch.rand(model_dim, v_dim, h_num,
+                              requires_grad=True)
+        self.o_w = torch.rand(h_num * v_dim, model_dim,
+                              requires_grad=True)
 
     def forward(self, query, key, value):
         """ forward 
@@ -42,10 +44,14 @@ class MultiheadAttention(nn.Module):
             key: j x d_k tensor
             value: j x d_v tensor
         """
+        print("Multihead1", key.size(), query.size(), value.size())
+        print("Multihead3", self.k_w.size(), self.v_w.size(), self.q_w.size())
         weighted_q = torch.einsum("im,mhk->ihk", query, self.q_w)
         weighted_k = torch.einsum("jm,mhk->jhk", key, self.k_w)
         weighted_v = torch.einsum("jm,mhv->jhv", value, self.v_w)
+
         heads = Attention(weighted_q, weighted_k, weighted_v)
+        print(heads.view(-1, self.h_num*self.v_dim).size())
         return torch.einsum("it,to->io", heads.view(-1, self.h_num*self.v_dim), self.o_w)
 
 
@@ -74,10 +80,10 @@ class EncoderLayer(nn.Module):
         self.att_layer = MultiheadAttention(h_num, model_dim)
 
     def forward(self, x):
-        h = self.att_layer(x)+x
-        h = nn.LayerNorm(h)
+        h = self.att_layer(x, x, x)+x
+        h = nn.functional.layer_norm(h,h.size())
         h = self.ff_layer(h)+x
-        return nn.LayerNorm(h)
+        return nn.functional.layer_norm(h,h.size())
 
 
 class DecoderLayer(nn.Module):
@@ -97,8 +103,8 @@ B = torch.tensor(
      [[3, 2, 1], [-1, 2, 3]],
      [[3, 2, 1], [-1, 2, 3]]])
 
-print(torch.einsum("ij,klj->ikl", A, B))
-print(B.view(-1, 6))
+#print(torch.einsum("ij,klj->ikl", A, B))
+#print(B.view(-1, 6))
 
-test = FeedForward(10, 20, 10)
-print(test.forward(torch.rand(10, 10)))
+test = EncoderLayer(20, 10)
+print(test.forward(torch.rand(10, 10)).size())
