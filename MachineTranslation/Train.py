520f71c8198a5b7cd6b93f1acaeccd13daeeed5f
d-ageha <d.ageha.sekaijyu1041@gmail.com>
2023-06-14 01:47:26 +0900

    gonna be a conflict~

--- MachineTranslation/Train.py
+++ MachineTranslation/Train.py
@@ -10,10 +10,13 @@ from JESC_DataSet import JESC_DataSet
 
 
 class EtoJModel(torch.nn.Module):
-    def __init__(self, model_dim: int, en_pad_idx: int, ja_pad_idx: int, max_seq_len: int, en_embs: int, ja_embs: int) -> None:
+    def __init__(self, model_dim: int, en_pad_idx: int, ja_pad_idx: int, max_seq_len: int, en_embs: int, ja_embs: int, use_mine=True) -> None:
         super().__init__()
-        self.transformer = Transformer(6, 6, 8, model_dim, max_seq_len, 0.1)
-        #self.transformer = torch.nn.Transformer(model_dim, 8, batch_first=True)
+        self.use_mine = use_mine
+        if use_mine:
+            self.transformer = Transformer(6, 6, 8, model_dim, max_seq_len, 0.1)
+        else:
+            self.transformer = torch.nn.Transformer(model_dim, 8, batch_first=True)
         self.en_emb = torch.nn.Embedding(en_embs, model_dim, padding_idx=en_pad_idx)
         self.ja_emb = torch.nn.Embedding(ja_embs, model_dim, padding_idx=ja_pad_idx)
         self.linear = torch.nn.Linear(model_dim, ja_embs)
@@ -22,8 +25,10 @@ class EtoJModel(torch.nn.Module):
     def forward(self, x, y, x_pad_mask, y_pad_mask):
         x = self.en_emb(x)
         y = self.ja_emb(y)
-        out = self.transformer(x, y, x_pad_mask, y_pad_mask)
-        #out = self.transformer(x, y, src_key_padding_mask=x_pad_mask == 0, tgt_key_padding_mask=y_pad_mask == 0)
+        if self.use_mine:
+            out = self.transformer(x, y, x_pad_mask, y_pad_mask)
+        else:
+            out = self.transformer(x, y, src_key_padding_mask=x_pad_mask == 0, tgt_key_padding_mask=y_pad_mask == 0)
         out = self.linear(out)
         return out
 
@@ -36,27 +41,30 @@ class EtoJModel(torch.nn.Module):
     def ja_decode(self, y):
         return torch.argmax(y, 2)
 
-    def translate(self, x, sos_id, eos_id):
-        x = [self.en_emb(x)]
-        y = [[sos_id]]
-        y = self.ja_emb(y)
-        result = ""
-        for i in range(self.max_seq_len):
-            y = self.transformer(x, y)
-            result = self.ja_decode(y)
+    def translate(self, x, sos_id, eos_id, ja_pad_id, pad_mask):
+        result = torch.tensor([sos_id]).unsqueeze(0)
+        for i in range(self.max_seq_len - 1):
+            y_pads = torch.tensor([[ja_pad_id for j in range(self.max_seq_len - (i + 1))]])
+            y = torch.cat((result, y_pads), dim=1)
+            y_pad_mask = [1 for j in range(i + 1)] + [0 for j in range(self.max_seq_len - (i + 1))]
+            y_pad_mask = torch.tensor(y_pad_mask, dtype=torch.int64).unsqueeze(0)
+            if not self.use_mine:
+                pad_mask = pad_mask == 0
+                y_pad_mask = y_pad_mask == 0
+            y = self.forward(x, y, pad_mask, y_pad_mask)
+            next_token = torch.tensor([[self.ja_decode(y)[0, i]]])
+            result = torch.cat((result, next_token), dim=1)
+            print(result, result.shape)
             if result[0, -1] == eos_id:
                 break
         return result
-
-    def load_model(self, path):
-        self.transformer = torch.load(path)
 
 
 def train(train: str, val: str, dim=256, epoch=10, batch=1, lr=0.01):
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     torch.cuda.empty_cache()
     print(device)
-    max_length = 70
+    max_length = 130
     train_dataset = JESC_DataSet(train, max_length)
     val_dataset = JESC_DataSet(val, max_length)
     train_dataloader = DataLoader(train_dataset, batch, True)
@@ -69,7 +77,7 @@ def train(train: str, val: str, dim=256, epoch=10, batch=1, lr=0.01):
     model.to(device)
 
     optim = torch.optim.Adam(model.parameters(), lr=lr)
-    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=ja_pad_id, label_smoothing=0.1)
+    loss_fn = torch.nn.CrossEntropyLoss(ignore_index=ja_pad_id, label_smoothing=0.1, reduction="sum")
     for e in range(epoch):
         model.train(True)
         train_loss = 0.0
@@ -89,33 +97,23 @@ def train(train: str, val: str, dim=256, epoch=10, batch=1, lr=0.01):
             optim.zero_grad()
             out = model(en_tokens, ja_tokens, en_masks, ja_masks)
             loss = loss_fn(out.transpose(1, 2)[:, :, :-1], ja_tokens[:, 1:])
-            train_loss += float(loss)
             loss.backward()
             optim.step()
+            model.train(False)
             with torch.no_grad():
                 t.set_postfix_str("Epoch: {} loss={}".format(e, loss))
-            if i % 100 == 0:
-                out_tokens = model.ja_decode(out).to(device)
-                out_tokens = out_tokens.masked_fill(ja_masks == 0, ja_pad_id)
-                print(out_tokens[0])
-                print(train_dataset.ja_tokenizer.decode(out_tokens[0]))
-                print(train_dataset.ja_tokenizer.decode(ja_tokens[0]))
-                torch.save(model.state_dict(), "output/JESC_Transformer_Model")
-
-        train_loss /= train_dataloader.__len__()
-        model.train(False)
-
-        for i, data in tqdm(enumerate(val_dataloader), total=len(val_dataloader)):
-            en, ja = data["en"]["input_ids"].to(device), data["ja"]["input_ids"].to(device)
-            out = model(en)
-            loss = loss_fn(out, ja)
-            valid_loss += float(loss)
-        valid_loss /= val_dataloader.__len__()
-        print("Train Loss:{}, Valid Loss:{}".format(train_loss, valid_loss))
+                if i % 100 == 0:
+                    out_tokens = model.ja_decode(out).to(device)
+                    out_tokens = out_tokens.masked_fill(ja_masks == 0, ja_pad_id)
+                    print(out_tokens[0])
+                    print(train_dataset.ja_tokenizer.decode(out_tokens[0]))
+                    print(train_dataset.ja_tokenizer.decode(ja_tokens[0][1:]))
+                    print(ja_tokens[0])
+                    torch.save(model.state_dict(), "output/JESC_Transformer_Model")
 
     return model
 
 
 if __name__ == "__main__":
     print(torch.__version__)
-    model = train("dataset/train_p", "dataset/dev_p", 128, 10, 5, lr=1)
+    model = train("dataset/train_p", "dataset/dev_p", 728, 2, 10, lr=0.0001)
---
+++ MachineTranslation/translation_test.py
@@ -0,0 +1,28 @@
+import sys
+import torch
+from Train import EtoJModel
+from JESC_DataSet import JESC_DataSet
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("usage: {} dim filename mode".format(sys.argv[0]))
+        exit()
+    dim = int(sys.argv[1])
+    filename = sys.argv[2]
+    use_mine = sys.argv[3] == "mine"
+    dataset = JESC_DataSet("dataset/dev_p", 130)
+    en_pad_id = dataset.en_tokenizer.pad_token_id or 0
+    ja_pad_id = dataset.ja_tokenizer.pad_token_id or 0
+    model = EtoJModel(dim, en_pad_id, ja_pad_id, 130, len(
+        dataset.en_tokenizer), len(dataset.ja_tokenizer), use_mine=use_mine)
+    model.load_state_dict(torch.load(filename))
+    while True:
+        en = input()
+        en = dataset.en_tokenizer([en], padding="max_length", max_length=130)
+        en_tokens = torch.tensor(en["input_ids"])
+        en_pad_mask = torch.tensor(en["attention_mask"])
+        res = model.translate(en_tokens, dataset.ja_tokenizer.cls_token_id,
+                              dataset.ja_tokenizer.sep_token_id, ja_pad_id, en_pad_mask)
+        print(res)
+        print(dataset.en_tokenizer.decode(en_tokens[0]))
+        print(dataset.ja_tokenizer.decode(res[0]))
